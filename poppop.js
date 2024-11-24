4bfccfba2ba68b454536116b4720b80b9e80e0ef
Asadullah-jutt <asadullahjutt100@gmail.com>
2024-11-24 14:59:29 +0500

    Updated UI

--- poppop.js
+++ poppop.js
@@ -1,18 +1,6 @@
 const marksForm = document.getElementById("grand-marks");
 marksForm.addEventListener("submit", handleMarksFormSubmit);
 
-<<<<<<< Updated upstream
-const feedbackForm = document.getElementById("feedback-form");
-feedbackForm.addEventListener("submit", handleFeedbackFormSubmit);
-
-=======
-<<<<<<< HEAD
-=======
-const feedbackForm = document.getElementById("feedback-form");
-feedbackForm.addEventListener("submit", handleFeedbackFormSubmit);
-
->>>>>>> 4af6593ce95599fc76c604b48e2e9309447bce58
->>>>>>> Stashed changes
 const gpaCalculatorForm = document.getElementById("gpa-calculator");
 gpaCalculatorForm.addEventListener("submit", handleCalculatorFormSubmit);
 
@@ -50,38 +38,7 @@ async function handleCalculatorFormSubmit(event) {
   chrome.scripting.executeScript({ target: { tabId: tab.id }, function: calculatorMainFunction });
 }
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 
-=======
->>>>>>> Stashed changes
-async function handleFeedbackFormSubmit(event) {
-  event.preventDefault();
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  let url;
-  if (tab?.url) {
-    try {
-      url = new URL(tab.url);
-      if (url.hostname !== "flexstudent.nu.edu.pk") {
-        alert("Please open the FlexStudent website first.");
-        return;
-      }
-    } catch {}
-  }
-
-  const input = document.querySelector('input[name="feedback-radio"]:checked');
-  if (!input) {
-    alert("Please select a feedback option first.");
-    return;
-  }
-
-  chrome.scripting.executeScript({ target: { tabId: tab.id }, function: feedbackMainFunction, args: [input.value] });
-}
-<<<<<<< Updated upstream
-=======
->>>>>>> 4af6593ce95599fc76c604b48e2e9309447bce58
->>>>>>> Stashed changes
 
 async function marksMainFunction() {
   if (!window.location.href.includes("Student/StudentMarks")) {
